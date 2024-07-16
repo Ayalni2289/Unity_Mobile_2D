149afc3db74f64f9c6864742244dc93e425b64b0
Ayalni2289 <alininspor@hotmail.com>
2024-07-16 11:51:17 +0300

    We can collect carrot's

--- RabbitGame/Assets/Scripts/Player.cs
+++ RabbitGame/Assets/Scripts/Player.cs
@@ -5,10 +5,13 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
 
+    public GameObject winText;
+    int socre=0;
     public FixedJoystick joystick;
     public float speed;
     float hinput;
     float vinput;
+    public int winScore=5;
     // Start is called before the first frame update
     void Start()
     {
@@ -21,5 +24,15 @@ public class Player : MonoBehaviour
         hinput = joystick.Horizontal*speed*Time.deltaTime;
         vinput = joystick.Vertical*speed*Time.deltaTime;
         transform.Translate(hinput,vinput,0);
+    }
+    private void OnCollisionEnter2D(Collision2D collision) {
+        
+        if(collision.gameObject.tag== "Carrot"){
+            socre++;
+            Destroy(collision.gameObject) ;
+            if(socre>=winScore){
+                winText.SetActive(true);
+            }
+        }
     }
 }
